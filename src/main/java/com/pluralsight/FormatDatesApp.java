891de033e15bd1409280c632b37862d5859fecb4
sgrinley <shamar808@yahoo.com>
2026-04-23 16:28:06 -0700

    1st Format: MM/DD/YYYY

--- src/main/java/com/pluralsight/FormatDatesApp.java
+++ src/main/java/com/pluralsight/FormatDatesApp.java
@@ -1,7 +1,35 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class FormatDatesApp {
     public static void main(String[] args) {
+
+//      Get Current Local Date/Time (my system time)
+        LocalDateTime now = LocalDateTime.now();
+
+//      1st Format: MM/DD/YYYY
+        DateTimeFormatter f1 = DateTimeFormatter.ofPattern("MM/dd/yyyy ");
+        System.out.println("1: " + now.format(f1));
+
+
+
+
+
+
+
+
+
+
+//       2nd Format: MM/dd/yyyy
+//       3rd Format: yyyy-MM-dd
+//       4th Format: EEEE dd, yyyy
+//       5th Format: EEEE, MM, dd, yyyy HH:mm
+//       6th Format: EEEE, MM, dd, yyyy HH:mm:ss
+//       7th Format: h:mm 'on' dd-MM-yyyy
+//
 
     }
 }
