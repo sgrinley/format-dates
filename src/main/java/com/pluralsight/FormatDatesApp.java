a594b77a8b1cca83a95d33758dbffdd420cd6331
sgrinley <shamar808@yahoo.com>
2026-04-23 16:52:00 -0700

    8th Format: h:mm a 'on' dd-MM-yyyy

--- src/main/java/com/pluralsight/FormatDatesApp.java
+++ src/main/java/com/pluralsight/FormatDatesApp.java
@@ -10,25 +10,31 @@ public class FormatDatesApp {
 //      Get Current Local Date/Time (my system time)
         LocalDateTime now = LocalDateTime.now();
 
+
 //      1st Format: MM/DD/YYYY
         DateTimeFormatter f1 = DateTimeFormatter.ofPattern("MM/dd/yyyy ");
         System.out.println("1: " + now.format(f1));
+
 
 //      2nd Format: dd/MM/yyyy
         DateTimeFormatter f2 = DateTimeFormatter.ofPattern("dd/MM/yyyy  ");
         System.out.println("2: " + now.format(f2));
 
+
 //      3rd Format: yyyy-MM-dd
         DateTimeFormatter f3 = DateTimeFormatter.ofPattern("yyyy-MM-dd  ");
         System.out.println("3: " + now.format(f3));
+
 
 //      4th Format: EEEE dd, yyyy
         DateTimeFormatter f4 = DateTimeFormatter.ofPattern("EEEE dd, yyyy  ");
         System.out.println("4: " + now.format(f4));
 
+
 //      5th Format: EEEE, MM, dd, yyyy HH:mm
         DateTimeFormatter f5 = DateTimeFormatter.ofPattern("EEEE, MM, dd, yyyy HH:mm  ");
         System.out.println("5: " + now.format(f5));
+
 
 //      6th Format: EEEE, MM, dd, yyyy HH:mm:ss a
         DateTimeFormatter f6 = DateTimeFormatter.ofPattern("EEEE, MM, dd, yyyy HH:mm:ss a  ");
@@ -44,5 +50,7 @@ public class FormatDatesApp {
         DateTimeFormatter f8 = DateTimeFormatter.ofPattern("h:mm a 'on' dd-MM-yyyy  ");
         System.out.println("8: " + now.format(f8));
 
+
     }
+
 }
